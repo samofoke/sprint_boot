e98f68e363fa8d159c8b5c5218ba94bf8a309340
samofoke <samofoke@student.wethinkcode.co.za>
2021-05-18 22:29:49 +0200

    sample api check

---
+++ checking-for-requests/src/main/java/com/javaproject/checkingforrequests/controllers/UrlCheckController.java
@@ -0,0 +1,44 @@
+package com.javaproject.checkingforrequests.controllers;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UrlCheckController {
+    private final String IS_IT_UP = "The site is up!";
+    private final String IS_IT_DOWN = "The site is down!";
+    private final String INCORRECT_URL = "The url is incorrrect";
+    
+    @GetMapping("/check")
+    public String getStatus(@RequestParam String url) {
+        String returnMessage = "";
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            
+            int responseCategory = conn.getResponseCode() / 100;
+            
+            if ( responseCategory != 2 || responseCategory != 3) {
+                returnMessage = IS_IT_DOWN;
+            }
+            else {
+                returnMessage = IS_IT_UP;
+            }
+        } catch (MalformedURLException e) {
+            returnMessage = INCORRECT_URL;
+        } catch (IOException e) {
+            returnMessage = IS_IT_DOWN;
+        }
+
+        return returnMessage;
+    }
+}
