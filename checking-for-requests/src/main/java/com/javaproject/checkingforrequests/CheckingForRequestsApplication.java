a5675a4e98df07204222edd6d2c6b59bbbf2721f
samofoke <samofoke@student.wethinkcode.co.za>
2021-05-18 21:20:23 +0200

    learning spring boot

---
+++ checking-for-requests/src/main/java/com/javaproject/checkingforrequests/CheckingForRequestsApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.checkingforrequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckingForRequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckingForRequestsApplication.class, args);
+	}
+
+}
---
+++ checking-for-requests/src/test/java/com/javaproject/checkingforrequests/CheckingForRequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.checkingforrequests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingForRequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
